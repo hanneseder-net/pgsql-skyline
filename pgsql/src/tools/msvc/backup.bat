2d5b6e3316bec1d77b1d17f4e4cc006bec33d458
Hannes Eder <hannes@hanneseder.net>
2007-09-18 15:26:11 +0000

    * minimized backup / restore dump data   * recreating skyline dataset on demand during restore
    git-svn-id: http://svn.hanneseder.net/skyline/trunk@180 9a9975c1-5a99-c240-8112-2887f72400be

--- pgsql/src/tools/msvc/backup.bat
+++ pgsql/src/tools/msvc/backup.bat
@@ -1,6 +1,10 @@
 set PGROOT=c:\pgsql
 set PGDATA=%PGROOT%\db
 
+rem remove tables which are recreated 
+rem befor running backup, use:
+rem select 'drop table ' || relname || ';' from pg_class where relname ~ '[ace][0-9]d[0-9]+.*';
+
 pg_ctl start -w
 pg_dumpall > dumpall.dump
 pg_ctl stop -w
---
+++ pgsql/src/tools/msvc/restore.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+BINDIR=/pgsql/bin
+
+$BINDIR/pg_ctl start -w
+$BINDIR/psql -d postgres < dumpall.dump
+
+for dist in a c e; do
+    for dim in 2 3 4; do
+	for n in 100 1000 10000 100000 1000000; do
+	    pg_sldg -$dist -d $dim -n $n -r -i | $BINDIR/psql
+	done
+    done
+done
+
+$dist=e
+$dim=1
+
+for n in 100 1000 10000 100000 1000000; do
+    pg_sldg -$dist -d $dim -n $n -r -i | $BINDIR/psql
+done
+
+
+$BINDIR/pg_ctl stop -w
+

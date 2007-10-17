73756974f55b1c3d120efef0862c42a52fa31a99
Hannes Eder <hannes@hanneseder.net>
2007-10-17 14:29:35 +0000

    * tidy up createwrapper.pl   * create the function in the default schema instead of pg_catalog
    git-svn-id: http://svn.hanneseder.net/skyline/trunk@276 9a9975c1-5a99-c240-8112-2887f72400be

--- randdataset/contrib/randdataset/createwrapper.pl
+++ randdataset/contrib/randdataset/createwrapper.pl
@@ -1,8 +1,15 @@
-#!/bin/perl
+#!/bin/perl -w
+# -*- Perl -*- tell emacs that it is a perl file
+#
+# Author: Hannes Eder <Hannes@HannesEder.net>
+#
+# Description: used to generate parts of randataset.sql.in
+#
 
-$maxdim = 20;
+use strict;
 
-@fields = ("id int");
+my $maxdim = 20;
+my @fields = ("id int");
 
 print <<EOF
 /*
@@ -11,7 +18,7 @@ print <<EOF
 EOF
     ;
 
-for ($i=1; $i <= $maxdim; ++$i)
+for (my $i=1; $i <= $maxdim; ++$i)
 {
     print "DROP FUNCTION IF EXISTS rds${i}d(text,int,int);\n";
     print "DROP TYPE IF EXISTS rand_dataset_${i}d;\n\n";
@@ -24,11 +31,11 @@ print <<EOF
 EOF
     ;
 
-for ($i=1; $i <= $maxdim; ++$i)
+for (my $i=1; $i <= $maxdim; ++$i)
 {
     push @fields, (($i > 1 && (($i-1) % 5 == 0)) ? "\n\t" : "") . " d${i} float";
 
-    # CREATE OR REPLACE TYPE not yet supported
+    # CREATE OR REPLACE TYPE not supported
     print "CREATE TYPE rand_dataset_${i}d AS\n\t(", join(",", @fields), ");\n\n";
 }
 
@@ -40,18 +47,18 @@ EOF
     ;
 
 @fields = ("id int");
-for ($i=1; $i <= $maxdim; ++$i)
+for (my $i=1; $i <= $maxdim; ++$i)
 {
     push @fields, (($i > 1 && (($i-1) % 5 == 0)) ? "\n\t   " : "") . " d${i} float";
 
-    $dt = "rds(" . join(",", @fields) . ")";
+    my $dt = "rds(" . join(",", @fields) . ")";
 
     print <<EOF
 CREATE OR REPLACE FUNCTION rds${i}d(disttype text, rows int, seed int)
 	RETURNS setof rand_dataset_${i}d
 	AS \$\$
 	SELECT *
-	FROM pg_catalog.pg_rand_dataset(\$1, ${i}, \$2, \$3) AS
+	FROM pg_rand_dataset(\$1, ${i}, \$2, \$3) AS
 	${dt}
 	\$\$ LANGUAGE SQL IMMUTABLE STRICT;
 

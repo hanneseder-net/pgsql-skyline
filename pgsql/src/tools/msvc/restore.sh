8de3a0c007d7423a852a60b9bfb66d8169f18912
Hannes Eder <hannes@hanneseder.net>
2008-03-05 11:12:37 +0000

    * use another table name schema for the test database
    git-svn-id: http://svn.hanneseder.net/skyline/trunk@470 9a9975c1-5a99-c240-8112-2887f72400be

--- pgsql/src/tools/msvc/restore.sh
+++ pgsql/src/tools/msvc/restore.sh
@@ -5,13 +5,16 @@ BINDIR=/pgsql/bin
 $BINDIR/pg_ctl start -w
 $BINDIR/psql -d postgres < dumpall.dump
 
+dim=15
+
 for dist in i c a; do
     for exp in `seq 1 6`; do
 	for mantissa in 1 5; do
 	    let n=mantissa*10**exp
 	    if (( n <= 1000000 )); then
 		for seed in `seq 0 9`; do
-		    randdataset -$dist -d 15 -n $n -s $seed -I -R | $BINDIR/psql
+		    tablename=${dist}${dim}d${mantissa}e${exp}s$seed
+		    randdataset -$dist -d $dim -n $n -s $seed -I -R -T $tablename | $BINDIR/psql
 		done
 	    fi
 	done
@@ -19,7 +22,7 @@ for dist in i c a; do
 done
 
 $BINDIR/psql <<EOF
-vacuum full verbose analyze;
+vacuum full analyze;
 EOF
 
 $BINDIR/pg_ctl stop -w

46abe569f7be46690f8a2da4abf123252fead4f7
Hannes Eder <hannes@hanneseder.net>
2008-03-05 10:35:41 +0000

    * database for performance experiments
    git-svn-id: http://svn.hanneseder.net/skyline/trunk@469 9a9975c1-5a99-c240-8112-2887f72400be

--- pgsql/src/tools/msvc/restore.sh
+++ pgsql/src/tools/msvc/restore.sh
@@ -5,21 +5,21 @@ BINDIR=/pgsql/bin
 $BINDIR/pg_ctl start -w
 $BINDIR/psql -d postgres < dumpall.dump
 
-for dist in a c i; do
-    for dim in 2 3 4; do
-	for n in 100 1000 10000 100000 1000000; do
-	    pg_sldg -$dist -d $dim -n $n -r -I | $BINDIR/psql
+for dist in i c a; do
+    for exp in `seq 1 6`; do
+	for mantissa in 1 5; do
+	    let n=mantissa*10**exp
+	    if (( n <= 1000000 )); then
+		for seed in `seq 0 9`; do
+		    randdataset -$dist -d 15 -n $n -s $seed -I -R | $BINDIR/psql
+		done
+	    fi
 	done
     done
 done
 
-dist=i
-dim=1
-
-for n in 100 1000 10000 100000 1000000; do
-    pg_sldg -$dist -d $dim -n $n -r -I | $BINDIR/psql
-done
-
+$BINDIR/psql <<EOF
+vacuum full verbose analyze;
+EOF
 
 $BINDIR/pg_ctl stop -w
-

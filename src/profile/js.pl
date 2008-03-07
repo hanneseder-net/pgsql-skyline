f05099d67fc4d0166e8bea792abcc5c444c5fae0
Hannes Eder <hannes@hanneseder.net>
2008-03-07 19:00:39 +0000

    * seems to work ... but it into action ;)
    git-svn-id: http://svn.hanneseder.net/skyline/trunk@478 9a9975c1-5a99-c240-8112-2887f72400be

--- src/profile/js.pl
+++ src/profile/js.pl
@@ -5,17 +5,23 @@ use Fcntl ':flock'; # import LOCK_* constants
 
 my $lockfile = "js.lock";
 my $jobpattern = "*.sql";
-my $pid = "$ENV{HOSTNAME}.$$";
+#my $pid = "$ENV{HOSTNAME}.$$";
+my $pid = $ENV{'HOSTNAME'};
+my $delay = 10;
+
+mkdir($pid) if (! -d $pid);
+mkdir($pid . ".done")  if (! -d ($pid . ".done"));
+mkdir("log") if (! -d "log");
 
 sub lock {
-    print "INFO: waiting for lock on \"$lockfile\"...\n";
+    print "INFO: waiting for lock on \"$lockfile\"...";
     flock(LOCKFILE,LOCK_EX);
-    print "INFO: got lock...\n";
+    print " [OK]\n";
 }
 
 sub unlock {
     flock(LOCKFILE,LOCK_UN);
-    print "INFO: released lock on \"$lockfile\"...\n";
+    print "INFO: released lock on \"$lockfile\"\n";
 }
 
 print "INFO: open lockfile \"$lockfile\" non exclusive...\n";
@@ -24,28 +30,36 @@ open(LOCKFILE, ">$lockfile") or die "Can't open lockfile $!";
 srand();
 
 while (1) {
+    print "INFO: sleep for a moment...\n";
+    sleep(int(rand($delay)));
+
     lock();
     my @jobs = glob $jobpattern;
     if ($#jobs < 0) {
-	print "INFO: no jobs found sleeping for a moment...\n";
-	sleep(int(rand(5)));	
+	print "INFO: no jobs found, sleeping for a moment...\n";
+	sleep(int(rand($delay)));
+	unlock();
     }
     else {
 	my $job = shift @jobs;
-	print "INFO: working on job $job...\n";
-	my $jobwip = $job . "." . $pid;
+	print "INFO: trying to dequeue job \"$job\"...\n";
+	my $jobwip = $pid . "/" . $job;
 	if (!rename($job, $jobwip)) {
 	    print "WARNING: failed to rename \"$job\" to \"$jobwip\"...\n";
-	    sleep(int(rand(5)));
+	    sleep(int(rand($delay)));
+	    unlock();
 	}
 	else {
-	    `cat $jobwip`;
-	    sleep(int(rand(5)));
-	    my $jobdone = $jobwip . ".done";
+	    unlock();
+	    print "INFO: fine, start working in job...\n";
+	    my $joblog = "log/" . $job . $pid . ".log";
+	    system "echo \"--<pid pid=$pid/>\" | cat - $jobwip | c:/pgsql/bin/psql -a >$joblog 2>&1" || die "Couldn't execute job $!";
+	    sleep(int(rand($delay)));
+	    my $jobdone = $pid . ".done/" . $job;
+	    print "INFO: done working on job \"$jobwip\".\n";
 	    rename($jobwip, $jobdone) || die "Can't rename file $!";
 	}
     }
-    unlock();
 }
 
 close(LOCKFILE);

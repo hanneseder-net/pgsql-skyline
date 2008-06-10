a005ea561c6c9447f5cf20ce7f177bf447feda48
Hannes Eder <hannes@hanneseder.net>
2008-06-10 18:50:30 +0000

    * prove of concept code, for bug in BNL
    git-svn-id: http://svn.hanneseder.net/skyline/trunk@651 9a9975c1-5a99-c240-8112-2887f72400be

---
+++ thesis/bnl/Program.cs
@@ -0,0 +1,125 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace bnl
+{
+    struct Tuple
+    {
+        public string id;
+        public int d1;
+        public int d2;
+        public int ts;
+
+        public Tuple(string id, int d1, int d2)
+        {
+            this.id = id;
+            this.d1 = d1;
+            this.d2 = d2;
+            this.ts = 0;
+        }
+
+        public bool dominates(Tuple q)
+        {
+            return d1 <= q.d1 && d2 <= q.d2 && (d1 < q.d1 || d2 < q.d2);
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int windowsize = 2;
+            Queue<Tuple> I = new Queue<Tuple>();
+
+            //I.Enqueue(new Tuple("a", 10, 1));
+            I.Enqueue(new Tuple("b1", 2, 10));
+            I.Enqueue(new Tuple("b2", 4, 8));
+            I.Enqueue(new Tuple("c", 10, 1));
+            I.Enqueue(new Tuple("d1", 1, 9));
+            I.Enqueue(new Tuple("d2", 3, 7));
+
+            List<Tuple> W = new List<Tuple>();
+            Queue<Tuple> T = new Queue<Tuple>();
+            Queue<Tuple> O = new Queue<Tuple>();
+
+            int tsIn = 0;
+            int tsOut = 0;
+            
+            while (I.Count > 0)
+            {
+                Propagate(W, O, tsIn);
+
+                Tuple p = I.Dequeue();
+                tsIn++;
+                p.ts = tsOut;
+                bool iscandiate = true;
+                
+            dominancecheck:
+                foreach (Tuple q in W)
+                {
+                    if (q.dominates(p))
+                    {
+                        iscandiate = false;
+                        break;
+                    }
+                    else if (p.dominates(q))
+                    {
+                        W.Remove(q);
+                        goto dominancecheck;
+                    }
+                }
+
+                if (iscandiate)
+                {
+                    if (W.Count < windowsize)
+                    {
+                        W.Add(p);
+                    }
+                    else
+                    {
+                        T.Enqueue(p);
+                        tsOut++;
+                    }
+                }
+
+                if (I.Count == 0)
+                {
+                    foreach (Tuple q in T)
+                    {
+                        I.Enqueue(q);
+                    }
+                    T.Clear();
+                    tsIn = 0;
+                    tsOut = 0;
+                }
+            }
+
+            foreach (Tuple q in W)
+            {
+                O.Enqueue(q);
+            }
+
+            foreach (Tuple q in O)
+            {
+                Console.WriteLine(q.id);
+            }
+            Console.WriteLine();
+
+        }
+
+        private static void Propagate(List<Tuple> W, Queue<Tuple> O, int tsIn)
+        {
+            propagte:
+            foreach (Tuple q in W)
+            {
+                if (q.ts == tsIn)
+                {
+                    O.Enqueue(q);
+                    W.Remove(q);
+                    goto propagte;
+                }
+            }
+        }
+    }
+}
